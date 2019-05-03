135691a17b587798bb5e73f3d6001e9fd1777363
jeremeheh <48673316+jeremeheh@users.noreply.github.com>
2019-05-03 16:07:30 +0800

    a

--- platforms/android/app/src/main/assets/www/js/custom-script.js
+++ platforms/android/app/src/main/assets/www/js/custom-script.js
@@ -1,6 +1,6 @@
 ons.ready(function() {
   ons.enableDeviceBackButtonHandler();
-
+  
   ons.setDefaultDeviceBackButtonListener(function(event) {
 
 });
@@ -27,15 +27,53 @@ window.fn.load = function (page) {
 document.addEventListener('init', function (event) {
 
   //Notif
-  if (event.target.id === 'notification') {
-    var title = event.target.data && event.target.data.title ? event.target.data.title : 'Notification';
-    event.target.querySelector('ons-toolbar div.center').textContent = title;
+  if(event.target.id === 'notifications'){
+              ons.ready(function() {
+            var pullHook = document.getElementById('pull-hook');
+
+            pullHook.addEventListener('changestate', function(event) {
+              var message = '';
+
+              switch (event.state) {
+                case 'initial':
+                message = 'Pull to refresh';
+                break;
+                case 'preaction':
+                message = 'Release';
+                break;
+                case 'action':
+                message = '<ons-icon icon="fa-redo" spin style="color:#1DA1F2" size="20px"></ons-icon>';
+                break;
+              }
+
+              pullHook.innerHTML = message;
+            });
+
+            pullHook.onAction = function(done) {
+              setTimeout(done, 2000);
+              setTimeout(getNotif(), 2000);
+
+            };
+          });
+
+          function getNotif(){
+            $.getJSON("https://www.gcccsbsit.xyz/_gpark/_api/select/s_Notif.php", function(data){
+              var notifs ="";
+              for(var i = 0; i < data.length; i++){
+                notifs += '<ons-list-item style="background-color: white;" modifier="chevron" tappable>';
+                notifs += '<div class="left"><img class="list-item__thumbnail" src="img/no-image.png"></div>';
+                notifs += '<div class="center"><span class="list-item__title">'+data[i].n_title+'</span><span class="list-item__subtitle" id="content">'+data[i].n_content+'</span></div>';
+                notifs += '</ons-list-item>';
+              }
+
+              document.getElementById("ons-list").innerHTML = notifs;
+
+            });
+
+          }
+          getNotif();
   }
 
-  if (event.target.id === 'visited') {
-    var title = event.target.data && event.target.data.title ? event.target.data.title : 'Visited';
-    event.target.querySelector('ons-toolbar div.center').textContent = title;
-  }
 
   if (event.target.id === 'home') { 
     initMap();
